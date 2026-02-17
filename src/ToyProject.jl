0f25812e2ce06f631837a77cebcf4477bb42e045
chrhum <christoph.hummel@gmx.ch>
2026-02-17 13:06:33 +0100

    Initial implementation of angle with tests and reproducible environment

--- src/ToyProject.jl
+++ src/ToyProject.jl
@@ -1,5 +1,47 @@
 module ToyProject
 
 # Write your package code here.
+using LinearAlgebra
+
+import Base: angle
+
+# export angle
+
+# """
+#     angle(u, v)
+
+# Return the angle between vectors `u` and `v` in radians.
+# """
+# function angle(u::AbstractVector, v::AbstractVector)
+#     cosθ = dot(u, v) / (norm(u) * norm(v))
+#     cosθ = clamp(cosθ, -1.0, 1.0)
+#     return acos(cosθ)
+# end
+
+
+# import Base: angle    # <--- WICHTIG
+
+"""
+    angle(u, v)
+
+Return the angle between vectors `u` and `v` in radians.
+
+Throws an error if either vector is zero.
+"""
+function angle(u::AbstractVector{T}, v::AbstractVector{T}) where {T<:Real}
+    nu = norm(u)
+    nv = norm(v)
+
+    if iszero(nu) || iszero(nv)
+        error("Angle undefined for zero vector.")
+    end
+
+    cosθ = dot(u, v) / (nu * nv)
+    cosθ = clamp(cosθ, -one(T), one(T))
+
+    return acos(cosθ)
+end
+
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,8 @@ using Test
 
 @testset "ToyProject.jl" begin
     # Write your tests here.
+
+@test angle([1.0, 0.0], [0.0, 1.0]) ≈ π/2
+@test angle([1.0, 0.0], [1.0, 0.0]) ≈ 0.0
+   
 end
